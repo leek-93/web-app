17101d2fe6cf52dfed40bddf007601bb9a4c9b0c
squeaksleeks <squeaksleeks@gmail.com>
2023-09-11 23:47:59 -0500

    intro-share-gallarypages

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('image'));
+
+
 app. listen(8080, function(){
     console.log('open');
 });
 
 app.get('/', function(request, response){
     response.sendFile(__dirname+'/index.html');
+});
+
+app.get('/intro', function(request, response){
+    response.sendFile(__dirname+'/intro.html');
+});
+
+app.get('/share', function(request, response){
+    response.sendFile(__dirname+'/share.html');
+});
+
+app.get('/gallary', function(request, response){
+    response.sendFile(__dirname+'/gallary.html');
 });
 
 app.post('/', function(request, response){
